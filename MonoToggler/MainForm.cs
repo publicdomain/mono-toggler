dcf8b2d1b29d930231680d5867fb5b43d54b3fc3
Victor <publicdomainvic@gmail.com>
2022-08-11 18:04:51 -0400

    Add LoadSettingsFile()

--- MonoToggler/MainForm.cs
+++ MonoToggler/MainForm.cs
@@ -52,6 +52,8 @@ namespace MonoToggler
         {
             // The InitializeComponent() call is required for Windows Forms designer support.
             this.InitializeComponent();
+
+
         }
 
         /// <summary>
@@ -172,6 +174,24 @@ namespace MonoToggler
         private void OnRefreshToolStripMenuItemClick(object sender, EventArgs e)
         {
             // TODO Add code
+        }
+
+        /// <summary>
+        /// Loads the settings file.
+        /// </summary>
+        /// <returns>The settings file.</returns>
+        /// <param name="settingsFilePath">Settings file path.</param>
+        private SettingsData LoadSettingsFile(string settingsFilePath)
+        {
+            // Use file stream
+            using (FileStream fileStream = File.OpenRead(settingsFilePath))
+            {
+                // Set xml serialzer
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(SettingsData));
+
+                // Return populated settings data
+                return xmlSerializer.Deserialize(fileStream) as SettingsData;
+            }
         }
 
         /// <summary>
